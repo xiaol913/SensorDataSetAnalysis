4fdc28b8404da61a8b4b8f0014e3651ce3dee1fd
Shawn Gao <xxlangdou@gmail.com>
2017-03-02 20:40:48 -0600

    fix

--- CombineData.py
+++ CombineData.py
@@ -7,9 +7,11 @@ def read_data(files, activity):
     #                 'GravityX', 'GravityY', 'GravityZ', 'timestamp', 'Activity']
     column_names = ['accelerometerX', 'accelerometerY', 'accelerometerZ', 'GyroscopeX', 'GyroscopeY', 'GyroscopeZ',
                     'GravityX', 'GravityY', 'GravityZ', 'Activity']
+    # column_names = ['accelerometerX', 'accelerometerY', 'accelerometerZ', 'GyroscopeX', 'GyroscopeY', 'GyroscopeZ',
+                    # 'GravityX', 'GravityY', 'GravityZ']
     df = pd.DataFrame()
     # n = 0
-    for i in range(0, len(files)):
+    for i in range(0, len(files)/2):
         df1 = pd.read_csv(files[i], header=None, names=column_names)
         for j in range(0, len(df1)):
             # n += 20
@@ -26,19 +28,20 @@ InVehicle = glob.glob("./InVehicle/*")
 Still = glob.glob("./Still/*")
 Walking = glob.glob("./Walking/*")
 
-# InVehicleData = read_data(InVehicle, 0)
-# StillData = read_data(Still, 1)
-# WalkingData = read_data(Walking, 2)
-
 InVehicleData = read_data(InVehicle, 0)
-StillData = read_data(Still, 0)
-WalkingData = read_data(Walking, 0)
-#
+StillData = read_data(Still, 1)
+WalkingData = read_data(Walking, 2)
+
+# InVehicleData = read_data(InVehicle, 0)
+# StillData = read_data(Still, 0)
+# WalkingData = read_data(Walking, 0)
+
 newData = pd.DataFrame()
 newData = newData.append(InVehicleData)
 newData = newData.append(StillData)
 newData = newData.append(WalkingData)
 
 newData.to_csv('all_data.csv')
-newData.to_csv('all_data_without_header_train.csv', header=None)
-newData.to_csv('all_data_without_header_test.csv', header=None)
+# newData.to_csv('all_data_without_header_train.csv', header=None)
+# newData.to_csv('all_data_without_header_test.csv', header=None)
+# newData.to_csv('all_data_test.csv')
--- TestData.py
+++ TestData.py
@@ -2,9 +2,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn import linear_model
+import sklearn.preprocessing as preprocessing
 
 # 绘图使用 ggplot 的 style
-plt.style.use('ggplot')
+# plt.style.use('ggplot')
 
 
 # 数据标准化
@@ -14,32 +16,32 @@ def feature_normalize(dataset):
     return (dataset - mu) / sigma
 
 
-# 绘图
-def plot_axis(ax, x, y, title):
-    ax.plot(x, y)
-    ax.set_title(title)
-    ax.xaxis.set_visible(False)
-    ax.set_ylim([min(y) - np.std(y), max(y) + np.std(y)])
-    ax.set_xlim([min(x), max(x)])
-    ax.grid(True)
-
-
-# 为给定的行为画出一段时间（180 × 50ms）的波形图
-def plot_activity(activity, data):
-    fig, (ax0, ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8) = plt.subplots(nrows=9, figsize=(15, 10), sharex=True)
-    plot_axis(ax0, data['timestamp'], data['accelerometerX'], 'accelerometerX')
-    plot_axis(ax1, data['timestamp'], data['accelerometerY'], 'accelerometerY')
-    plot_axis(ax2, data['timestamp'], data['accelerometerZ'], 'accelerometerZ')
-    plot_axis(ax3, data['timestamp'], data['GyroscopeX'], 'GyroscopeX')
-    plot_axis(ax4, data['timestamp'], data['GyroscopeY'], 'GyroscopeY')
-    plot_axis(ax5, data['timestamp'], data['GyroscopeZ'], 'GyroscopeZ')
-    plot_axis(ax6, data['timestamp'], data['GravityX'], 'GravityX')
-    plot_axis(ax7, data['timestamp'], data['GravityY'], 'GravityY')
-    plot_axis(ax8, data['timestamp'], data['GravityZ'], 'GravityZ')
-    plt.subplots_adjust(hspace=0.2)
-    fig.suptitle(activity)
-    plt.subplots_adjust(top=0.90)
-    plt.show()
+# # 绘图
+# def plot_axis(ax, x, y, title):
+#     ax.plot(x, y)
+#     ax.set_title(title)
+#     ax.xaxis.set_visible(False)
+#     ax.set_ylim([min(y) - np.std(y), max(y) + np.std(y)])
+#     ax.set_xlim([min(x), max(x)])
+#     ax.grid(True)
+#
+#
+# # 为给定的行为画出一段时间（180 × 50ms）的波形图
+# def plot_activity(activity, data):
+#     fig, (ax0, ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8) = plt.subplots(nrows=9, figsize=(15, 10), sharex=True)
+#     plot_axis(ax0, data['timestamp'], data['accelerometerX'], 'accelerometerX')
+#     plot_axis(ax1, data['timestamp'], data['accelerometerY'], 'accelerometerY')
+#     plot_axis(ax2, data['timestamp'], data['accelerometerZ'], 'accelerometerZ')
+#     plot_axis(ax3, data['timestamp'], data['GyroscopeX'], 'GyroscopeX')
+#     plot_axis(ax4, data['timestamp'], data['GyroscopeY'], 'GyroscopeY')
+#     plot_axis(ax5, data['timestamp'], data['GyroscopeZ'], 'GyroscopeZ')
+#     plot_axis(ax6, data['timestamp'], data['GravityX'], 'GravityX')
+#     plot_axis(ax7, data['timestamp'], data['GravityY'], 'GravityY')
+#     plot_axis(ax8, data['timestamp'], data['GravityZ'], 'GravityZ')
+#     plt.subplots_adjust(hspace=0.2)
+#     fig.suptitle(activity)
+#     plt.subplots_adjust(top=0.90)
+#     plt.show()
 
 
 data_set = pd.read_csv('all_data.csv')
@@ -54,7 +56,36 @@ data_set['GravityX'] = feature_normalize(data_set['GravityX'])
 data_set['GravityY'] = feature_normalize(data_set['GravityY'])
 data_set['GravityZ'] = feature_normalize(data_set['GravityZ'])
 
+data_test = pd.read_csv('all_data_test.csv')
+
+data_test['accelerometerX'] = feature_normalize(data_test['accelerometerX'])
+data_test['accelerometerY'] = feature_normalize(data_test['accelerometerY'])
+data_test['accelerometerZ'] = feature_normalize(data_test['accelerometerZ'])
+data_test['GyroscopeX'] = feature_normalize(data_test['GyroscopeX'])
+data_test['GyroscopeY'] = feature_normalize(data_test['GyroscopeY'])
+data_test['GyroscopeZ'] = feature_normalize(data_test['GyroscopeZ'])
+data_test['GravityX'] = feature_normalize(data_test['GravityX'])
+data_test['GravityY'] = feature_normalize(data_test['GravityY'])
+data_test['GravityZ'] = feature_normalize(data_test['GravityZ'])
+
 # 为每个行为绘图
-for activity in np.unique(data_set["Activity"]):
-    subset = data_set[data_set["Activity"] == activity][:800]
-    plot_activity(activity, subset)
+# for activity in np.unique(data_set["Activity"]):
+#     subset = data_set[data_set["Activity"] == activity][:800]
+#     plot_activity(activity, subset)
+
+# print data_test
+
+train = data_set.filter(regex='Activity|accelerometer*|Gyroscope*|Gravity*')
+train_np = train.values
+y = train_np[:, -1]
+x = train_np[:, :-1]
+
+# print x
+
+clf = linear_model.LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
+clf.fit(x, y)
+
+test = data_test.filter(regex='accelerometer*|Gyroscope*|Gravity*')
+predictions = clf.predict(test)
+result = pd.DataFrame({'Activity': predictions.astype(np.int32)})
+result.to_csv('result.csv', index=False)
