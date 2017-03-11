d09eb683ebb53c6ede4e9ccd988ed39712d987d2
Shawn Gao <xxlangdou@gmail.com>
2017-03-11 03:10:28 -0600

    Python side finished

--- PMMLTrainData.py
+++ PMMLTrainData.py
@@ -2,8 +2,11 @@ import pandas as pd
 from sklearn2pmml import PMMLPipeline
 from sklearn2pmml.decoration import ContinuousDomain
 from sklearn_pandas import DataFrameMapper
-from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import RidgeClassifier
+from sklearn.preprocessing import Imputer
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectKBest
 from sklearn.externals import joblib
 from sklearn2pmml import sklearn2pmml
 import numpy as np
@@ -14,13 +17,15 @@ result_data = pd.read_csv('result_data.csv')
 
 train_pipeline = PMMLPipeline([
     ("mapper", DataFrameMapper([
-        (["AccelerometerX", "AccelerometerY", "AccelerometerZ"], [ContinuousDomain(), Imputer()])
+        (["AccelerometerX", "AccelerometerY", "AccelerometerZ"], [ContinuousDomain(), StandardScaler()])
     ])),
-    ("classifier", RidgeClassifier(alpha=0.001, fit_intercept=True, normalize=True,
-                                   copy_X=True, max_iter=1000, tol=0.000001,
+    ("pca", PCA(n_components=3)),
+    ("selector", SelectKBest(k=3)),
+    ("classifier", RidgeClassifier(alpha=0.01, fit_intercept=True, normalize=False,
+                                   copy_X=True, max_iter=100, tol=0.001,
                                    class_weight=None, solver='auto', random_state=None))
 ])
-train_pipeline.fit(train_data, train_data['Activity'])
+train_pipeline = train_pipeline.fit(train_data, train_data['Activity'])
 test = test_data.filter(regex='Accelerometer')
 
 predictions = train_pipeline.predict(test)
@@ -37,5 +42,6 @@ print(rate)
 
 if rate > 0.86:
     result.to_csv('result.csv', index=False)
+    # joblib.dump(train_pipeline, "newPKL.pkl.z", compress=9)
     # save as PMML
     sklearn2pmml(train_pipeline, "RidgeClassifier.pmml", with_repr=True)
